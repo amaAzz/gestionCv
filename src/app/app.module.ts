7ce56d0306daf0c30518faf77e28654507d00ac4
abdelkarim <Ahaddoudi74@gmail.com>
2024-05-21 20:45:48 +0100

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,9 @@ import { RegistreCompanyComponent } from './registre-company/registre-company.co
 import { AddJobComponent } from './add-job/add-job.component';
 import {CompanyNavbarComponent} from "./company-navbar/company-navbar.component";
 import { PostulationJobComponent } from './postulation-job/postulation-job.component';
+import {ListPostulationComponent} from "./list-postulation/list-postulation.component";
+import {CommonModule} from "@angular/common";
+import {PostulationCandidatComponent} from "./postulation-candidat/postulation-candidat.component";
 
 @NgModule({
   declarations: [
@@ -40,7 +43,9 @@ import { PostulationJobComponent } from './postulation-job/postulation-job.compo
     RegistreCompanyComponent,
     AddJobComponent,
     CompanyNavbarComponent,
-    PostulationJobComponent
+    PostulationJobComponent,
+    ListPostulationComponent,
+    PostulationCandidatComponent
 
   ],
   imports: [
@@ -49,6 +54,7 @@ import { PostulationJobComponent } from './postulation-job/postulation-job.compo
     HttpClientModule,
     ReactiveFormsModule,
     FormsModule,
+    CommonModule
 
   ],
   providers: [],
